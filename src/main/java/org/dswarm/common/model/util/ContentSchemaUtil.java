00b3f611b1453398eda2361b276dbc2bfa875b71
Thomas Gängler <tgaengler@avantgarde-labs.de>
2015-06-16 10:17:48 +0200

    added license headers

--- src/main/java/org/dswarm/common/model/util/ContentSchemaUtil.java
+++ src/main/java/org/dswarm/common/model/util/ContentSchemaUtil.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Copyright (C) 2013 – 2015 SLUB Dresden & Avantgarde Labs GmbH (<code@dswarm.org>)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.dswarm.common.model.util;
 
 /**
